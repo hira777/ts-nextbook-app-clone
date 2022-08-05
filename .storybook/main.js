fbabe1f3f34b32b72c19011ef31eb15bc1476846
soarflat <soarflat777@gmail.com>
2022-08-06 00:43:25 +0900

    Storybook に Theme を適用した

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,34 @@
+const path = require('path')
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
+    '@storybook/addon-postcss',
   ],
   framework: '@storybook/react',
   // Storybook 用の静的ファイルを設置するディレクトリ
   staticDirs: ['public'],
+  babel: async (options) => ({
+    ...options,
+    plugins: [
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-proposal-private-methods',
+      '@babel/plugin-proposal-private-property-in-object',
+    ],
+  }),
+  // Storybook のデフォルトの webpack の設定を上書きする。
+  // config にはデフォルトの設定が渡される。
+  webpackFinal: async (config) => {
+    // tsconfig.json に指定してあるエイリアスを webpack.config.js の alias プロパティに追加する。
+    config.resolve.plugins = [
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, '../tsconfig.json'),
+      }),
+    ]
+
+    return config
+  },
 }
