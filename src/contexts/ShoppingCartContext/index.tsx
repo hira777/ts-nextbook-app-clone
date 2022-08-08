10eb1c704f2e46d38b9a8360d8af334f1ac74c14
soarflat <soarflat777@gmail.com>
2022-08-09 00:46:43 +0900

    Header コンポーネントを追加

---
+++ src/contexts/ShoppingCartContext/index.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { shopReducer, ADD_PRODUCT, REMOVE_PRODUCT } from './reducers'
+import type { Product } from 'types'
+
+type ShoppingCartContextType = {
+  cart: Product[]
+  addProductToCart: (product: Product) => void
+  removeProductFromCart: (productId: number) => void
+}
+
+const ShoppingCartContext = React.createContext<ShoppingCartContextType>({
+  cart: [],
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  addProductToCart: () => {},
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  removeProductFromCart: () => {},
+})
+
+export const useShoppingCartContext = (): ShoppingCartContextType =>
+  React.useContext<ShoppingCartContextType>(ShoppingCartContext)
+
+interface ShoppingCartContextProviderProps {
+  children?: React.ReactNode
+}
+
+export const ShoppingCartContextProvider = ({
+  children,
+}: ShoppingCartContextProviderProps) => {
+  const products: Product[] = []
+  const [cartState, dispatch] = React.useReducer(shopReducer, products)
+
+  // 商品をカートに追加
+  const addProductToCart = (product: Product) => {
+    dispatch({ type: ADD_PRODUCT, payload: product })
+  }
+
+  // 商品をカートから削除
+  const removeProductFromCart = (productId: number) => {
+    dispatch({ type: REMOVE_PRODUCT, payload: productId })
+  }
+
+  return (
+    <ShoppingCartContext.Provider
+      value={{
+        cart: cartState,
+        addProductToCart,
+        removeProductFromCart,
+      }}
+    >
+      {children}
+    </ShoppingCartContext.Provider>
+  )
+}
